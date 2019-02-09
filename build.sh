3d544c5f555d5b098979f8c51a1d17546779bf36
Your Name <you@example.com>
2019-02-09 15:00:46 -0600

    189ddd

--- build.sh
+++ build.sh
@@ -15,3 +15,21 @@ cd ./aports/scripts
 
 mkdir ../../iso
 sh mkimage.sh --tag edge --outdir ../../iso --arch x86_64 --repository http://dl-cdn.alpinelinux.org/alpine/edge/main --profile standard
+
+cd ../../iso
+
+bucket="isos"
+file="alpine-standard-edge-x86_64.iso"
+
+resource="/${bucket}/${file}"
+content_type="application/octet-stream"
+date=`date -R`
+_signature="PUT\n\n${content_type}\n${date}\n${resource}"
+signature=`echo -en ${_signature} | openssl sha1 -hmac ${s3_secret} -binary | base64`
+
+curl -v -X PUT -T "${file}" \
+          -H "Host: $host" \
+          -H "Date: ${date}" \
+          -H "Content-Type: ${content_type}" \
+          -H "Authorization: AWS ${s3_key}:${signature}" \
+          http://$host${resource}
